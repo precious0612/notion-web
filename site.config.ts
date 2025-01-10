3fea74c00638b5abc3c05f926116f9affe39d8b7
Cyrus Bentley <cyrus.bentley@icloud.com>
2025-01-10 17:57:24 +0800

    Update site.config.ts
    Set page for ChatBI demo files

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '177dfe87700e8085b839c92dd1649b8f',
+  rootNotionPageId: '177dfe87700e8037815bfc2b466d45cf',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
